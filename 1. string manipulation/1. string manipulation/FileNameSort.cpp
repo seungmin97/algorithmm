a75da259843c7a0557f4d97bbebe0bfaa6964b12
seungmin97 <lsm0341@naver.com>
2019-03-28 12:19:58 +0900

    change file

---
+++ 1. string manipulation/1. string manipulation/FileNameSort.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <stdlib.h>
+
+#define SIZE 1000
+
+using namespace std;
+
+class File {
+
+public:
+	string head;
+	int num;
+	string tail;
+};
+
+int main() {
+
+	int i, j;
+
+	string *FILE;
+	string str;
+	cin >> str;
+	int num = 0;
+
+	vector<string> FileNames = { "F-5 Freedom Fighter", "B-50 Superfortress", "A-10 Thunderbolt II", "F-14 Tomcat" };
+
+
+	{ "img12.png", "img10.png", "img02.png", "img1.png", "IMG01.GIF", "img2.JPG" };
+
+//	for (i = 0; i < FileName.size(); i++) {
+
+	}
+
+	return 0;
+}
---
+++ 1. string manipulation/1. string manipulation/GroupWordChecker.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+
+	int num = 0;
+	int i, j;
+	int result;
+
+	cin >> num;
+
+	result = num;
+	string str;
+
+	for (i = 0; i < num; i++) {
+
+		int alphabet[27] = { 0 };
+
+		cin >> str;
+
+		alphabet[(int)str[0] - 97] = 1;
+
+		for (j = 1; j < str.length(); j++) {
+
+			//	cout << (int)str[0];
+
+			if (str[j] != str[j - 1]) {
+
+				if (alphabet[(int)str[j] - 97] == 1) {
+					result -= 1;
+					break;
+				}
+				else {
+					alphabet[(int)str[j] - 97] = 1;
+				}
+			}
+		}
+	}
+
+	cout << result;
+	return 0;
+
+}
