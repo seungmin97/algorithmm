54b7bb80f564c31a162c440985b1e305648ac34d
이승민 <lsm0341@naver.com>
2019-04-15 13:10:39 +0900

    problem 1

--- 5. DataStructure/Security Algorithm.cpp
+++ 5. DataStructure/Security Algorithm.cpp
@@ -1,11 +1,45 @@
 #include <iostream>
 #include <algorithm>
 #include <vector>
+#include <string>
 
 using namespace std;
 
+class security{
+public:
+	vector <string> v1;
+	vector <string> v2;
+	vector <string> v3;
+
+	void Detoxification();
+};
+
 int main() {
 
+	int num;
+	int loop = 0;
+	cin >> loop;
+	
+	vector <security> v;
+
+	string temp;
+	for (int i = 0; i < loop; i++) {
+		cin >> num;
+		for (int j = 0; j < num; j++) {
+			cin >> temp;
+			v[i].v1.push_back(temp);
+		}
+		for (int j = 0; j < num; j++) {
+			cin >> temp;
+			v[i].v2.push_back(temp);
+		}
+		for (int j = 0; j < num; j++) {
+			cin >> temp;
+			v[i].v3.push_back(temp);
+		}
+	}
+
+	
 
 	return 0;
 }
