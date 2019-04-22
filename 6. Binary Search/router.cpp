63e45762382dbda6c1a97e0d4fd91f1dac47dc1c
이승민 <lsm0341@naver.com>
2019-04-22 16:07:14 +0900

    problem 1

--- 6. Binary Search/router.cpp
+++ 6. Binary Search/router.cpp
@@ -18,6 +18,37 @@ int main() {
 		v.push_back(temp);
 	}
 
+	sort(v.begin(), v.end());
+	int left = 1;
+	int right = v.at(num_home - 1);
+	int num = 0;
 
+	int start = 0;
+
+	int size = (right - 1) / 2;
+
+	while (size >= 1) {
+		start = 0;
+		num = 1;
+		for (int i = 1; i < num_home; i++) {
+
+			if (v.at(i) - v.at(start) >= size) {
+				start = i;
+				num++;
+			}
+		}
+
+		if (num > num_router) {
+			size += 1;
+		}
+		else if (num == num_router) {
+			cout << num << endl ;
+			return 0;
+		}
+		else {
+			size -= 1;
+		}
+	}
+	
 	return 0;
 }
