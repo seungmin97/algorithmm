da61c3194b1dc3bc828b3e4cf929cf4a7f2d102d
이승민 <lsm0341@naver.com>
2019-04-24 10:09:50 +0900

    problem 2

---
+++ 6. Binary Search/budget.cpp
@@ -0,0 +1,58 @@
+//https://www.acmicpc.net/problem/2512
+
+#include <iostream>
+#include <algorithm>
+#include <vector>
+
+using namespace std;
+
+int main() {
+
+	int num;
+	cin >> num;
+
+	vector <int> v(num);
+
+	int max = 0;
+	for (int i = 0; i < num; i++) {
+		cin >> v[i];
+
+		if (v[i] > max) {
+			max = v[i];
+		}
+	}
+	
+	int budget;
+	cin >> budget;
+
+	int left = 0;
+	int right = max;
+	int result = 0;
+
+	while (left <= right) {
+		int mid = (left + right) / 2;
+		long long sum = 0;
+
+		for (int i = 0; i < num; i++) {
+			if (v[i] < mid) {
+				sum += v[i];
+			}
+			else {
+				sum += mid;
+			}
+		}
+
+		if (sum <= budget) {
+			left = mid + 1;
+			result = mid;
+		}
+		else {
+			right = mid - 1;
+		}
+
+	}
+
+	cout << result<<endl;
+
+	return 0;
+}
