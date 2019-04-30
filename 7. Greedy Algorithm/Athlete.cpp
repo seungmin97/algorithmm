48b64101cdf5b32ab7326fc7bccb9a8a3da0326b
이승민 <lsm0341@naver.com>
2019-04-30 15:51:58 +0900

    problem 2

---
+++ 7. Greedy Algorithm/Athlete.cpp
@@ -0,0 +1,76 @@
+//https://algospot.com/judge/problem/read/MATCHORDER
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+class Score {
+
+private:
+	vector <int> Russia;
+	vector <int> Korea;
+	int size;
+public:
+	int result = 0;
+	Score(vector <int> Russia, vector <int> Korea, int size) {
+		sort(Russia.begin(), Russia.end());
+		sort(Korea.begin(), Korea.end());
+
+		this->Russia = Russia;
+		this->Korea = Korea;
+		this->size = size;
+	}
+	void check_how_many_win();
+};
+
+void Score::check_how_many_win() {
+	int index = 0;
+	for (int i = 0; i < size; i++) {
+		int r = Russia[i];
+		for (int j = index; j < size; j++) {
+			int k = Korea[j];
+			if (r <= k) {
+				index = j + 1;
+				result += 1;
+				break;
+			}
+		}
+	}
+}
+
+int main() {
+
+	int num;
+	cin >> num;
+
+	vector<Score> s;
+
+	vector <int> Russia;
+	vector <int> Korea;
+	int size = 0;
+
+	for (int i = 0; i < num; i++) {
+
+		cin >> size;
+		for (int j = 0; j < size; j++) {
+			int temp;
+			cin >> temp;
+			Russia.push_back(temp);
+		}
+		for (int j = 0; j < size; j++) {
+			int temp;
+			cin >> temp;
+			Korea.push_back(temp);
+		}
+		s.push_back(Score(Russia, Korea, size));
+		s[i].check_how_many_win();
+	}
+
+	for (int i = 0; i < num; i++) {
+		cout << s[i].result << endl;
+	}
+
+	return 0;
+}
---
+++ 7. Greedy Algorithm/Rope.cpp
@@ -0,0 +1,30 @@
+//https://www.acmicpc.net/problem/2217
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+
+	int num;
+	cin >> num;
+
+	vector <int> v(num);
+
+	for (int i = 0; i < num; i++) {
+		cin >> v[i];
+	}
+
+	sort(v.begin(), v.end());
+
+	int ans = 0;
+	for (int i = 0; i < num; i++) {
+		
+		ans = max(ans, v[i] * (num - i));
+	}
+
+	cout << ans<<endl;
+	return 0;
+}
