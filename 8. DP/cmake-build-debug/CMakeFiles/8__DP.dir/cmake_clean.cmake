c323b9238d9fbe044497561b5aaa9a343c5460c3
seungmin97 <lsm0341@naver.com>
2019-05-05 15:49:46 +0900

    week8

---
+++ 8. DP/cmake-build-debug/CMakeFiles/8__DP.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/8__DP.dir/main.cpp.o"
+  "8__DP.pdb"
+  "8__DP"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/8__DP.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
