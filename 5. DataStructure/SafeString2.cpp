4022486d8302ad13b2f88d9bb3d0de7e0a9020fd
seungmin97 <lsm0341@naver.com>
2019-05-05 09:48:24 +0900

    problem 2 푸는중

--- 5. DataStructure/SafeString2.cpp
+++ 5. DataStructure/SafeString2.cpp
@@ -7,8 +7,17 @@
 #include <stack>
 #include <string>
 #include <algorithm>
+#include <vector>
 
 using namespace std;
+
+vector <int> v;
+
+void print(){
+    for (int i = 0; i < v.size(); ++i) {
+        cout << i + 1 << ". " << v.at(i) << endl;
+    }
+}
 
 int main() {
 
@@ -19,14 +28,15 @@ int main() {
        count = 0;
 
        stack<char> s;
-       // cin >> str;
-       str = "{}{}";
+        cin >> str;
         if(str.find('-') != -1)
         {
+            print();
             return 0;
         }
         else{
             for (int i = 0; i < str.length(); ++i) {
+                //첫번째 값
                 if(i == 0){
                     if(str[i] == '}'){
                         count += 1;
@@ -36,16 +46,24 @@ int main() {
                         s.push(str[i]);
                     }
                 }
+                //나머지 값
                 else{
                     if(str[i] == '{'){
                         s.push(('{'));
                     }
-                    else {
-                        s.top() == '{';
-                        s.pop();
+                    else{
+                        if(s.top() == '{'){
+                            s.pop();
+                        }
+                        else{
+                            s.push('{');
+                            count += 1;
+                        }
                     }
                 }
             }
         }
+        count += s.size() / 2;
+        v.push_back(count);
     }
 }
